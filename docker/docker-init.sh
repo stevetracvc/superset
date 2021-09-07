209e3ebe5fa286c86169b63f8fc450da61917477
Steve Marek <steve@tracvc.com>
2021-09-07 09:57:30 -0600

    initial trac config stuff

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -52,14 +52,14 @@ superset db upgrade
 echo_step "1" "Complete" "Applying DB migrations"
 
 # Create an admin user
-echo_step "2" "Starting" "Setting up admin user ( admin / $ADMIN_PASSWORD )"
-superset fab create-admin \
-              --username admin \
-              --firstname Superset \
-              --lastname Admin \
-              --email admin@superset.com \
-              --password $ADMIN_PASSWORD
-echo_step "2" "Complete" "Setting up admin user"
+#echo_step "2" "Starting" "Setting up admin user ( admin / $ADMIN_PASSWORD )"
+#superset fab create-admin \
+#              --username admin \
+#              --firstname Superset \
+#              --lastname Admin \
+#              --email admin@superset.com \
+#              --password $ADMIN_PASSWORD
+#echo_step "2" "Complete" "Setting up admin user"
 # Create default roles and permissions
 echo_step "3" "Starting" "Setting up roles and perms"
 superset init
