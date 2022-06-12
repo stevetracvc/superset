fd772d2924cbd84cb6ba939b2414e5aab77bbd93
Steve Marek <steve@tracvc.com>
2022-06-12 20:51:59 +0000

    cache changes

--- docker/pythonpath_dev/superset_config_docker.py
+++ docker/pythonpath_dev/superset_config_docker.py
@@ -131,10 +131,14 @@ SMTP_MAIL_FROM = "admin@tracvc.com"
 
 CACHE_CONFIG = {
     'CACHE_TYPE': 'redis',
-    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 24, # 1 day default (in secs)
+    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 24 * 30, # 30 day (in secs)
     'CACHE_KEY_PREFIX': 'superset_results',
 #    'CACHE_REDIS_URL': 'redis://localhost:6379/0',
     'CACHE_REDIS_URL': 'redis://redis:6379/0',
+}
+DATA_CACHE_CONFIG = {
+    **CACHE_CONFIG,
+    'CACHE_KEY_PREFIX': 'superset_results_data',
 }
 
 
